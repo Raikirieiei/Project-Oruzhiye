90b76cbe7dc3f3e1a64742a17f590d79ff962905
KongtappV <kongtapp.v@ku.th>
2023-04-06 17:14:18 +0700

    add map3

--- Assets/Scripts/Shop/Shopkeeper.cs
+++ Assets/Scripts/Shop/Shopkeeper.cs
@@ -5,16 +5,62 @@ using UnityEngine;
 
 public class Shopkeeper : MonoBehaviour
 {
+    [Header("Shop")]
+
+    [Header("Detecting Player")]
+    [SerializeField] Vector2 lineOfSight;
+    [SerializeField] LayerMask playerLayer;
+    private bool canSeePlayer;
+
+    [Header("Others")]
+    [SerializeField] Transform player;
+    private bool facingRight = false;
 
     void Start() {
-      
+        Invoke(nameof(Find_player), 1);
+        if (player == null) return;
     }
 
     void FixedUpdate() {
-        AnimationController();
+        canSeePlayer = Physics2D.OverlapBox(transform.position, lineOfSight, 0, playerLayer);
+
+        if (canSeePlayer)
+        {
+            FlipTowardsPlayer();
+        }
     }
 
-    void AnimationController()
+    private void Find_player()
     {
+        try
+        {
+            player = GameObject.FindWithTag("Player").transform;
+        }
+        catch (NullReferenceException)
+        {
+            Debug.Log("target gameObjects is not present in hierarchy ");
+        }
+    }
+
+    void FlipTowardsPlayer()
+    {
+        float playerPosition = player.position.x - transform.position.x;
+
+        if (playerPosition < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            transform.Rotate(0, 180, 0);
+        }
+        else if (playerPosition > 0 && !facingRight)
+        {
+            facingRight = !facingRight;
+            transform.Rotate(0, 180, 0);
+        }
+    }
+    private void OnDrawGizmosSelected() 
+    {
+        // LineOfSight marker
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, lineOfSight);
     }
 }
