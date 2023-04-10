a4b1be47b01d15bedc679d4bdda97ac7aca54ceb
KongtappV <kongtapp.v@ku.th>
2023-04-10 11:43:16 +0700

    finish shadow hand entity for BoD

--- Assets/Scripts/Enemy/Ground Enemy/BoD/ShadowHand.cs
+++ Assets/Scripts/Enemy/Ground Enemy/BoD/ShadowHand.cs
@@ -5,5 +5,39 @@ using UnityEngine;
 
 public class ShadowHand : MonoBehaviour
 {
-    
+    [Header("Basic Attribute")]
+    [SerializeField] int damage;
+    private bool collideOnce = false;
+
+    [Header("Player Detection")]
+    [SerializeField] Transform player;
+    [SerializeField] LayerMask playerLayer;
+
+    void Start()
+    {
+        Invoke(nameof(Find_player), 0.25f);
+        if (player == null) return;
+    }
+
+    private void Find_player()
+    {
+        try
+        {
+            player = GameObject.FindWithTag("Player").transform;
+        }
+        catch (NullReferenceException)
+        {
+            Debug.Log("target gameObjects is not present in hierarchy ");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      if (!collideOnce)
+      {
+        Player playerScript = player.GetComponent<Player>();
+        playerScript.TakeDamage(damage, new Vector2(0f, 0f));
+        collideOnce = true;
+      }
+    }
 }
